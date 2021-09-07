968a5d8bf21cf813dcfe99cbee06b090f742d862
L.Lu <luliangce@gmail.com>
2021-09-07 18:19:21 +0800

    add

---
+++ precache-manifest.fb75e433b2e6dc046d6fcae5877b4108.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb6cb370762d3f69302",
+    "url": "/security-simulator/css/app.7580dba3.css"
+  },
+  {
+    "revision": "abe71f7d608d43b56d9b2aef78d7ae99",
+    "url": "/security-simulator/fonts/element-icons.abe71f7d.ttf"
+  },
+  {
+    "revision": "d9491be2c5109fca0fa40d0c59e2e3b9",
+    "url": "/security-simulator/fonts/element-icons.d9491be2.woff"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/security-simulator/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d330e8bc98613307a9bf7f836627c87b",
+    "url": "/security-simulator/index.html"
+  },
+  {
+    "revision": "0271111964017b50178d",
+    "url": "/security-simulator/js/about.c3677961.js"
+  },
+  {
+    "revision": "8fb6cb370762d3f69302",
+    "url": "/security-simulator/js/app.7eb9f21b.js"
+  },
+  {
+    "revision": "56667b4590eeedb24659",
+    "url": "/security-simulator/js/chunk-vendors.6058e4ec.js"
+  },
+  {
+    "revision": "0e56dd5237a5f332ee76947deb849d17",
+    "url": "/security-simulator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/security-simulator/robots.txt"
+  }
+]);
